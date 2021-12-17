5e6fe1bc3e199a1f7ba7329f65399e31185ed05a
Matías Giusti <matigiusti2002@gmail.com>
2021-12-16 23:04:30 -0300

    .

---
+++ ElBuenDino/src/ObjetosJuego/AdministradorEnemigos.java
@@ -0,0 +1,71 @@
+package ObjetosJuego;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import Utilidades.Recursos;
+
+public class AdministradorEnemigos {
+	
+	private BufferedImage cactus1;
+	private BufferedImage cactus2;
+	private Random rand;
+	
+	private List<Enemigos> enemigos;
+	private Personaje personaje;
+	
+	public AdministradorEnemigos(Personaje mainCharacter) {
+		rand = new Random();
+		cactus1 = Recursos.getResouceImage("data/cactus1.png");
+		cactus2 = Recursos.getResouceImage("data/cactus2.png");
+		enemigos = new ArrayList<Enemigos>();
+		this.personaje = mainCharacter;
+		enemigos.add(createEnemigo());
+	}
+	
+	public void update() {
+		for(Enemigos e : enemigos) {
+			e.update();
+		}
+		Enemigos enemy = enemigos.get(0);
+		if(enemy.isOutOfScreen()) {
+			personaje.upScore();
+			enemigos.clear();
+			enemigos.add(createEnemigo());
+		}
+	}
+	
+	public void draw(Graphics g) {
+		for(Enemigos e : enemigos) {
+			e.draw(g);
+		}
+	}
+	
+	private Enemigos createEnemigo() {
+		// if (enemyType = getRandom)
+		int type = rand.nextInt(2);
+		if(type == 0) {
+			return new Cactus(personaje, 800, cactus1.getWidth() - 10, cactus1.getHeight() - 10, cactus1);
+		} else {
+			return new Cactus(personaje, 800, cactus2.getWidth() - 10, cactus2.getHeight() - 10, cactus2);
+		}
+	}
+	
+	public boolean isCollision() {
+		for(Enemigos e : enemigos) {
+			if (personaje.getBound().intersects(e.getBound())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void reset() {
+		enemigos.clear();
+		enemigos.add(createEnemigo());
+	}
+	
+}
---
+++ ElBuenDino/src/ObjetosJuego/Nubes.java
@@ -0,0 +1,74 @@
+package ObjetosJuego;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import interfazDelUsuario.Ventana;
+import Utilidades.Recursos;
+
+public class Nubes {
+	private List<ImageCloud> listNube;
+	private BufferedImage nube;
+	
+	private Personaje personaje;
+	
+	public Nubes(int width, Personaje mainCharacter) {
+		this.personaje = mainCharacter;
+		nube = Recursos.getResouceImage("data/cloud.png");
+		listNube = new ArrayList<ImageCloud>();
+		
+		ImageCloud imageCloud = new ImageCloud();
+		imageCloud.posX = 0;
+		imageCloud.posY = 30;
+		listNube.add(imageCloud);
+		
+		imageCloud = new ImageCloud();
+		imageCloud.posX = 150;
+		imageCloud.posY = 40;
+		listNube.add(imageCloud);
+		
+		imageCloud = new ImageCloud();
+		imageCloud.posX = 300;
+		imageCloud.posY = 50;
+		listNube.add(imageCloud);
+		
+		imageCloud = new ImageCloud();
+		imageCloud.posX = 450;
+		imageCloud.posY = 20;
+		listNube.add(imageCloud);
+		
+		imageCloud = new ImageCloud();
+		imageCloud.posX = 600;
+		imageCloud.posY = 60;
+		listNube.add(imageCloud);
+	}
+	
+	public void update(){
+		Iterator<ImageCloud> itr = listNube.iterator();
+		ImageCloud firstElement = itr.next();
+		firstElement.posX -= personaje.getSpeedX()/8;
+		while(itr.hasNext()) {
+			ImageCloud element = itr.next();
+			element.posX -= personaje.getSpeedX()/8;
+		}
+		if(firstElement.posX < -nube.getWidth()) {
+			listNube.remove(firstElement);
+			firstElement.posX = Ventana.SCREEN_WIDTH;
+			listNube.add(firstElement);
+		}
+	}
+	
+	public void draw(Graphics g) {
+		for(ImageCloud imgLand : listNube) {
+			g.drawImage(nube, (int) imgLand.posX, imgLand.posY, null);
+		}
+	}
+	
+	private class ImageCloud {
+		float posX;
+		int posY;
+	}
+}
+
